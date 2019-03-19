f26083378afd007d17c723a85054cb6f92ca32a3
win <win@LAPTOP-KCJQEUVG.lan>
2019-03-19 21:18:01 +0800

    update sql

--- springboot-demo Maven Webapp/doc/demo_2019-03-18.sql
+++ springboot-demo Maven Webapp/doc/demo_2019-03-18.sql
@@ -1,48 +1,77 @@
-create database demo;
-use demo;
+-- MySQL dump 10.13  Distrib 5.7.19, for Win32 (AMD64)
+--
+-- Host: localhost    Database: demo
+-- ------------------------------------------------------
+-- Server version	5.7.19-log
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `t_class`
+--
 
 DROP TABLE IF EXISTS `t_class`;
-
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `t_class` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `class_name` varchar(100) DEFAULT NULL,
   `student_num` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `t_class`
+--
 
 LOCK TABLES `t_class` WRITE;
 /*!40000 ALTER TABLE `t_class` DISABLE KEYS */;
-
-INSERT INTO `t_class` (`id`, `class_name`, `student_num`)
-VALUES
-	(1,'class_1',0),
-	(2,'class_2',0);
-
+INSERT INTO `t_class` VALUES (1,'class_1',0),(2,'class_2',0);
 /*!40000 ALTER TABLE `t_class` ENABLE KEYS */;
 UNLOCK TABLES;
 
+--
+-- Table structure for table `t_user`
+--
 
 DROP TABLE IF EXISTS `t_user`;
-
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `t_user` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `name` varchar(50) CHARACTER SET utf8 DEFAULT NULL,
   `age` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `t_user`
+--
 
 LOCK TABLES `t_user` WRITE;
 /*!40000 ALTER TABLE `t_user` DISABLE KEYS */;
-
-INSERT INTO `t_user` (`id`, `name`, `age`)
-VALUES
-	(1,'张三',20),
-	(2,'李四',25),
-	(4,'王五',20),
-	(5,'赵六',25),
-	(11,'周七',22);
-
+INSERT INTO `t_user` VALUES (1,'张三',20),(2,'李四',25),(4,'王五',20),(5,'赵六',25),(11,'周七',22);
 /*!40000 ALTER TABLE `t_user` ENABLE KEYS */;
 UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
+-- Dump completed on 2019-03-19 21:16:27
