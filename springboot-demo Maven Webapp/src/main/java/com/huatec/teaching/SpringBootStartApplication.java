57c839f91f26c2efeee0af0cfbccbfd79bad5f92
jiabo <jia.bo@huatec.com>
2019-05-11 11:24:23 +0800

    代码格式化

--- springboot-demo Maven Webapp/src/main/java/com/huatec/teaching/SpringBootStartApplication.java
+++ springboot-demo Maven Webapp/src/main/java/com/huatec/teaching/SpringBootStartApplication.java
@@ -3,10 +3,11 @@ package com.huatec.teaching;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /**
  * springboot 项目启动类
- * @author 贾波
  *
+ * @author 贾波
  */
 //@SpringBootApplication声明让spring boot自动给程序进行必要的配置
 @SpringBootApplication
@@ -14,16 +15,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @MapperScan("com.huatec.teaching.dao")
 public class SpringBootStartApplication {
 
-	
-	
-	public static void main(String[] args) {
-		SpringApplication.run(SpringBootStartApplication.class, args);
-		System.out.println(" ______                    _   ______  \n" + 
-				"|_   _ \\                 |_|_   _ `.          \n" + 
-				"  | |_) |   .--.    .--. `| |-' | | `. \\  .--.   \n" + 
-				"  |  __'. / .'`\\ \\/ .'`\\ \\| |   | |  | |/ .'`\\ \\\n" + 
-				" _| |__) || \\__. || \\__. || |, _| |_.' /| \\__. | \n" + 
-				"|_______/  '.__.'  '.__.' \\__/|______.'  '.__.'  ");
-	}
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootStartApplication.class, args);
+        System.out.println(" ______                    _   ______  \n" +
+                "|_   _ \\                 |_|_   _ `.          \n" +
+                "  | |_) |   .--.    .--. `| |-' | | `. \\  .--.   \n" +
+                "  |  __'. / .'`\\ \\/ .'`\\ \\| |   | |  | |/ .'`\\ \\\n" +
+                " _| |__) || \\__. || \\__. || |, _| |_.' /| \\__. | \n" +
+                "|_______/  '.__.'  '.__.' \\__/|______.'  '.__.'  ");
+    }
 
 }
